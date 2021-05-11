cf3e1460381145fbca20da88da501df2c62cc07c
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-11 11:28:08 -0500

    hook up createPost action to form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,14 +2,20 @@ import React, {useState} from 'react';
 import useStyles from './Styles'; 
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'; 
+import {useDispatch} from 'react-redux'; 
+import {createPost} from '../../actions/posts'
 
 const Form = () => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags: 
         '', selectedFile: ''})
     const classes = useStyles(); 
-    const handleSubmit = () => {
+    const dispatch = useDispatch(); 
 
+    const handleSubmit = () => {
+        e.preventDefault(); 
+
+        dispatch(createPost(postData)); 
     }
     const clear = () => {
 

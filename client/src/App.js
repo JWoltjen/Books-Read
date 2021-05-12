0364f906abdf7e47840c8188c3a25a5da70e6fd9
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-12 12:37:01 -0500

    edit form field

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux'; 
 
 import { getPosts } from './actions/posts'
-import memories from './Images/memories.jpg'
+import books from './Images/books.jpeg'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'; 
@@ -19,8 +19,8 @@ const App = () => {
     return (
       <Container maxwidth='lg'>
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-            <img className={classes.image} src={memories} alt='memories' height="60" /> 
+            <Typography className={classes.heading} variant="h2" align="center">Books</Typography>
+            <img className={classes.image} src={books} alt='memories' height="60" /> 
         </AppBar>
         <Grow in>
             <Container>

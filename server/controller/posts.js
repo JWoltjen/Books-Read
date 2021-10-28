755f76df3850a5a4254b50397324f109207a53d3
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-10-28 12:09:08 -0500

    update dependencies

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -17,7 +17,7 @@ export const createPost = async (req, res) => {
         await newPost.save(); 
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({message: error.message})
+        res.status(404).json({message: error.message})
 }}
 
 
